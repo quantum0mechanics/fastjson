2fe3c12fd1e883ed79483f9480e915b1c4f90853
cpugputpu <cpugputpu@126.com>
2020-04-01 01:11:45 -0700

    Create mytest.java

---
+++ src/test/java/com/alibaba/json/bvt/issue_2900/mytest.java
@@ -0,0 +1,26 @@
+package com.alibaba.json.bvt.issue_2900;
+
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONArray;
+import com.alibaba.fastjson.JSONObject;
+import junit.framework.TestCase;
+import org.junit.Test;
+
+import java.util.*;
+
+public class mytest extends TestCase {
+
+
+    @Test
+    public void mytest()
+    {
+        HashSet<Object> test = new HashSet<>();
+        test.add(null);
+        test.add("123 ");
+        Iterator iter = test.iterator();
+        iter.next();
+        System.out.println(((String)(iter.next())).trim()); // if iter.next() is null, will throw NPE
+    }
+
+
+}
